4ea7589396b9e87b5ced5e2cdafee6cf0cb06875
Daniel Pittard <DPittard@otcxtrading.com>
2023-02-21 13:46:35 +0000

    examples

---
+++ tests/acceptance/stepDefinitions/addItems-Generated.js
@@ -0,0 +1,30 @@
+
+  Given('I write {string} into the task box', function (string) {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+
+  When('I click add', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+
+  Then('I want to see {string}', function (string) {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+
+  Given('I write {string} into the task box', function (string) {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+
+  When('I click add', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+
+  Then('I want to see {string}', function (string) {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
