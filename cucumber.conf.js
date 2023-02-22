4b5b3892beb377a39902873932c487c7dfcc62a1
Daniel Pittard <DPittard@otcxtrading.com>
2023-02-22 11:25:22 +0000

    config

--- cucumber.conf.js
+++ cucumber.conf.js
@@ -3,28 +3,27 @@ const { chromium } = require("playwright");
 
 setDefaultTimeout(60000)
 
-// Launch the browser
+// Before anything Launch the browser
 BeforeAll(async function () {
     global.browser = await chromium.launch({  
         headless: false, //hides or shows browser
-        slowMo: 2000, //slows down tests
+        slowMo: 1000, //slows down tests
     });
  });
- 
- // Close the browser
+
+ // Before a scenario create new page with context
+Before(async function () {
+   global.context = await global.browser.newContext();
+   global.page = await global.context.newPage();
+});
+
+// After a scenario close the page and close the context
+After(async function () {
+   await global.page.close();
+   await global.context.close();
+});
+
+ // After everything close the browser
  AfterAll(async function () {
     await global.browser.close();
  });
-
-// Create a new browser context and page per scenario
-Before(async function () {
-    global.context = await global.browser.newContext();
-    global.page = await global.context.newPage();
- });
- 
-// Cleanup after each scenario
- After(async function () {
-    await global.page.close();
-    await global.context.close();
- });
-
